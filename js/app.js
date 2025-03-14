b6b079066b8796c7632ecfabeb5d14f342abc119
pharelle123 <zeffelesra@gmail.com>
2025-03-14 12:53:27 +0100

    html css js

--- js/app.js
+++ js/app.js
@@ -3,20 +3,24 @@
 
     const inputName = document.querySelectorAll('input[type="texte"]')[0]
     const inputLastName = document.querySelectorAll('input[type="texte"]')[1]
-    console.log(inputLastName)
+    const age = document.getElementsByTagName("input")[2]
 
-    const btn_submit = document.getElementsByTagName('input')[2]
+    const btn_submit = document.getElementsByTagName("input")[3]
+
+    
+
    
     btn_submit.addEventListener("click",(e)=>{
       e.preventDefault()
       swal.fire({
 
-       title: "Good job!",
-       text: "You clicked the button!",
-      icon: "success"
-
+        title: "Good job!",
+        text: "You clicked the button!",
+       icon: "success"
+     
       })
 
+      
+
     })
-   
- })
+  })
